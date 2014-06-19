a63f4149145885147a0a0abf7435a406015da13f
Ippes <seppi.gol@gmail.com>
2014-06-19 17:49:50 +0200

    added counter and finite termination in scheduling executor

--- src/net/tomp2p/vdht/Executor.java
+++ src/net/tomp2p/vdht/Executor.java
@@ -24,14 +24,32 @@ public abstract class Executor implements Runnable {
 	private final ScheduledExecutorService scheduler;
 	private final int minDelayInMilliseconds;
 	private final int maxDelayInMilliseconds;
+	private final int numberExecutes;
 
-	public Executor(ScheduledExecutorService scheduler, int minDelayInMilliseconds, int maxDelayInMilliseconds)
-			throws IOException {
+	protected long counterExecutes = 0;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param scheduler
+	 *            the scheduler on which this Runnable proceeds to execute
+	 * @param minDelayInMilliseconds
+	 *            minimum delay between two schedules
+	 * @param maxDelayInMilliseconds
+	 *            maximum delay between two schedules
+	 * @param numberExecutes
+	 *            amount of total executions till it stops, <code>-1</code> for
+	 *            endless execution
+	 * @throws IOException
+	 */
+	public Executor(ScheduledExecutorService scheduler, int minDelayInMilliseconds,
+			int maxDelayInMilliseconds, int numberExecutes) throws IOException {
 		this.scheduler = scheduler;
 		this.minDelayInMilliseconds = minDelayInMilliseconds;
 		logger.trace("min put delay in milliseconds = '{}'", minDelayInMilliseconds);
 		this.maxDelayInMilliseconds = maxDelayInMilliseconds;
 		logger.trace("max put delay in milliseconds = '{}'", maxDelayInMilliseconds);
+		this.numberExecutes = numberExecutes;
 	}
 
 	public abstract void execute() throws Exception;
@@ -44,6 +62,16 @@ public abstract class Executor implements Runnable {
 		} catch (Exception e) {
 			logger.error("Caught an unexpected exception.", e);
 		} finally {
+			// check if a rescheduling is needed
+			if (counterExecutes < 0) {
+				return;
+			} else if (counterExecutes == numberExecutes) {
+				return;
+			} else {
+				// increase counter
+				counterExecutes++;
+			}
+			
 			// schedule next task with a varying delay
 			int delay = delay();
 			logger.trace("scheduling in '{}' milliseconds.", delay);
